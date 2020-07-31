f8571af995ca12652c2e9a5b1a9dc8b0bc378e3f
BuzzDyne <buzzdyne@gmail.com>
2020-07-31 21:49:01 +0700

    Implemented Faster queryPageScraping with BS4

--- TokpedScraper/main.py
+++ TokpedScraper/main.py
@@ -56,8 +56,9 @@ class ScrapeDriver:
         driver = webdriver.Chrome(chrome_path,options=opts, service_log_path='NULL.txt')
         return driver
         
-    def __init__(self):
-        self.driver = self.startupDriver()
+    def __init__(self, useSelenium=False):
+        if(useSelenium):
+            self.driver = self.startupDriver()
 
     def openUrl(self, url, watchXpath):
         self.driver.get(url)
@@ -147,21 +148,46 @@ class ScrapeDriver:
         page = requests.get(pObj.listingUrl, headers=header)
         soup = BeautifulSoup(page.text, 'html.parser')
 
+        lblListingName  = {'data-testid' : 'lblPDPDetailProductName'}
+        lblListingPrice = {'data-testid' : 'lblPDPDetailProductPrice'}
         lblListingStock = {'data-testid' : 'lblPDPDetailProductStock'}
+
         lblSoldCount    = {'data-testid' : 'lblPDPDetailProductSuccessRate'}
         lblSeenCount    = {'data-testid' : 'lblPDPDetailProductSeenCounter'}
+        
         lblReviewScore  = {'data-testid' : 'lblPDPDetailProductRatingNumber'}
         lblReviewCount  = {'data-testid' : 'lblPDPDetailProductRatingCounter'}
+        
         lblStoreName    = {'data-testid' : 'llbPDPFooterShopName'}
+        lblStoreArea    = {'data-testid' : 'lblPDPFooterLastOnline'}
 
-        # These data are NOT guaranteed to be there
+        
+        listingNameText = soup.find(attrs=lblListingName)
+        listingPriceText= soup.find(attrs=lblListingPrice)
         listingStockText= soup.find(attrs=lblListingStock)
+
         soldCountText   = soup.find(attrs=lblSoldCount)
         seenCountText   = soup.find(attrs=lblSeenCount)
+        
         reviewScoreText = soup.find(attrs=lblReviewScore)
         reviewCountText = soup.find(attrs=lblReviewCount)
+        
+        storeNameText   = soup.find(attrs=lblStoreName)
+        storeaAreaText  = soup.find(attrs=lblStoreArea)
 
-        # Convert Stock Text to Int
+        # Listing Name
+        if(listingNameText is None):
+            pObj.nameProd = "NoneType"
+        else:
+            pObj.nameProd = listingNameText.get_text()
+
+        # Listing Price
+        if(listingPriceText is None):
+            pObj.priceProd = defIntValue
+        else:
+            pObj.priceProd = myUtil.filterNonNumericToInt(listingPriceText.get_text()) 
+
+        # Stock
         if(listingStockText is None):
             pObj.listingStock = -1                              #NoneType
         else:
@@ -175,34 +201,41 @@ class ScrapeDriver:
             else:
                 pObj.listingStock = myUtil.filterNonNumericToInt(stock) #Limited
 
+        # Sold Count
         if(soldCountText is None):
             pObj.soldCount = defIntValue
         else:
             pObj.soldCount = myUtil.filterNonNumericToInt(soldCountText.get_text())
 
+        # Seen Count
         if(seenCountText is None):
             pObj.seenProduct = defIntValue
         else:
             pObj.seenProduct = myUtil.filterNonNumericToInt(seenCountText.get_text())
         
+        # Review Score
         if(reviewScoreText is None):
             pObj.reviewScore = defIntValue
         else:
             pObj.reviewScore = myUtil.filterNonNumericToInt(reviewScoreText.get_text())
         
+        # Review Count
         if(reviewCountText is None):
             pObj.reviewCount = defIntValue
         else:
             pObj.reviewCount = myUtil.filterNonNumericToInt(reviewCountText.get_text())
 
-        # This data IS guaranteed to be there
-        storeNameText   = soup.find(attrs=lblStoreName)
-        # pObj.storeName   = soup.find(attrs=lblStoreName).get_text()
-
+        # Store Name
         if(storeNameText is None):
             pObj.storeName = "NoneType"
         else:
             pObj.storeName = storeNameText.get_text()
+
+        # Store Area
+        if(storeaAreaText is None):
+            pObj.storeArea = "NoneType"
+        else:
+            pObj.storeArea = storeaAreaText.get_text()
 
         pObj.timestamp = int(time.time())
 
@@ -213,6 +246,7 @@ class ScrapeDriver:
 
         pList = []
 
+        # Extract listing data available from search page (Name, Price, Area, Url) and append ListingObj to listingObjList
         for p in products:
             pName = p.find_element_by_xpath(listingName_xpath).text
             pPrice = p.find_element_by_xpath(listingPrice_xpath).text
@@ -228,8 +262,9 @@ class ScrapeDriver:
 
         print("")
         
+        # Get the rest of missing listing data by visiting each listingUrl
         for p in pList:
-            self.getDataFromListingPageBS(p)
+            self.getDataFromListingPage(p)
             print(".", end="")
         
         print("")
@@ -238,15 +273,41 @@ class ScrapeDriver:
         # for p in pList:
         #     p.toString()
 
+    def scrapeProductBS(self, productQuery, header=None):
+        if(header == None):
+            header = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+            }
+
+        lblListingCard = {'data-testid' : "lnkProductContainer"}
+        
+        page = requests.get(searchUrl+productQuery, headers=header)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        # Create list of listingObj with only its own URL
+        pList = []
+        for a in soup.find_all(attrs=lblListingCard):
+            pList.append(listingObj(listingUrl=a['href']))
+            print(".", end="")
+        print("")
+
+        # Fetch the rest of listing data with its url
+        for p in pList:
+            self.getDataFromListingPageBS(p)
+            print(".", end="")
+        print("")
+
+        return pList
+
     def processQuery(self, qName):
-        pList = self.scrapeProduct(qName)
+        pList = self.scrapeProductBS(qName)
         myUtil.writeToCSV(qName, pList, time.time())
 
     def processListOfQuery(self, listQ):
         for p in listQ:
             self.processQuery(p)
 
-d = ScrapeDriver()
+d = ScrapeDriver(useSelenium=False)
 
 vgaList = [     "GTX 1650",
                 "GTX 1650 Super",
@@ -278,8 +339,8 @@ cpuList = [     "Ryzen 5 3600",
 
 start = time.time()
 
-# d.processListOfQuery(vgaList)
-d.processListOfQuery(cpuList)
+d.processListOfQuery(vgaList)
+# d.processListOfQuery(cpuList)
 
 end = time.time()
 print("Time: {}".format(end - start))
