1673267ba0f10ee6cbfb746f3fee6e1ec22b5e73
BuzzDyne <buzzdyne@gmail.com>
2020-10-14 21:17:52 +0700

    Implemented Logging
    All module activities will be logged to 'logs/' with csv format

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+import logging
+from datetime import datetime, timedelta
+import pytz
+
 from ScraperPackage.scraper_module import Scraper
 from FsPackage.fs_module import FsModule
+
+now = datetime.now(tz=pytz.timezone('Asia/Jakarta')).strftime("%Y-%m-%d_%H-%M-%S")
+LOG_FORMAT = "%(levelname)s,%(asctime)s.%(msecs)03d,%(module)s<%(funcName)s>,%(message)s"
+
+logging.basicConfig(
+  filename='logs/{}.csv'.format(now), 
+  level=logging.INFO, 
+  format=LOG_FORMAT,
+  datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
 
 class App:
     def __init__(self):
@@ -15,14 +29,22 @@ class App:
         5. Mark created 'NewListing'
       """
       newListingUrls = self.fs.getNewListingURLs()
+      newListingCount = len(newListingUrls)
 
-      for obj in newListingUrls:
+      if(newListingCount > 0) :
+        logging.info("Started scraping {} new Listing data...".format(newListingCount))
+        i = 1
+        for obj in newListingUrls:
           l = self.sc.scrapeInitialListing(obj.url)
 
           listingDocPath = self.fs.createListing(l.listingName, l.listingID, l.listingURL, l.storeName, l.storeArea)
           self.fs.insertSingleListingDataRow(l.listingID, l.dataRow)
           
           self.fs.setNewListingDocAddr(obj.selfDocAddr, listingDocPath)
+          logging.info("Successfully scraped {} of {} (ListingID:{})".format(i,newListingCount,l.listingID))
+          i += 1
+      else:
+        logging.info("No new ListingUrls found")
 
     def updateListingData(self):
       """
@@ -31,11 +53,25 @@ class App:
         3. create a new DataRow Doc inside each Listing Doc
       """
       listObj = self.fs.getExistingListingURLs()
+      countObj = len(listObj)
 
-      for obj in listObj:
+      if(countObj > 0):
+        logging.info("Started updating {} Listing(s)...".format(countObj))
+
+        i = 1
+        for obj in listObj:
           dataRow = self.sc.scrapeListingDataRow(obj.listingID)
           self.fs.insertSingleListingDataRow(obj.listingID, dataRow)
+          logging.info("Successfully updated {} of {} (ListingID:{})".format(i,countObj,obj.listingID))
+          i += 1
+      else:
+        logging.info("No ExistingListing needs to be updated")
 
-app = App()
-app.createNewListingData()
-app.updateListingData()
+def main():
+  app = App()
+
+  app.createNewListingData()
+  app.updateListingData()
+
+if __name__ == '__main__':
+  main()
